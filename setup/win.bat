2b8055f8aa1aed723ce320154486be3dd9a334b4
Y Okumura <y-okumura@nichiwa-system.co.jp>
2018-08-13 14:32:47 +0900

    windows用セットアップスクリプトを修正
    - edgeではconsoleにアクセスできないようなのでchromeもインストールするように修正
    - Microsoft-Windows-Subsystem-Linux は使わなかったのでインストール対象から除外
    
    windows subsystem Linux上にdocker入れてOKD入れて動くならそっちのほうが素直？要調査

--- setup/win.bat
+++ setup/win.bat
@@ -1,8 +1,6 @@
 powershell -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-cinst virtualbox minishift
+cinst virtualbox minishift GoogleChrome
 
 minishift start --vm-driver virtualbox
 minishift config set vm-driver virtualbox
-
-powershell -Command "Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -All"
